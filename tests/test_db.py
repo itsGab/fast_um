0d40da0ad8ace8c32514537e43a13316386f8808
Gabriel <gabriel.schultz10@gmail.com>
2025-06-06 18:50:07 -0300

    aula 4 - intro banco de dados e alembic migrations

---
+++ tests/test_db.py
@@ -0,0 +1,26 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fast_zero.models import User
+
+
+def test_create_user(session, mock_db_time):
+    """Test creating a new user."""
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='testuser', email='test@mail.com', password='password'
+        )
+        session.add(new_user)
+        session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'testuser'))
+
+    assert user.username == 'testuser'
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'testuser',
+        'email': 'test@mail.com',
+        'password': 'password',
+        'created_at': time,
+    }
